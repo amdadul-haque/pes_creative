df01ee15812b6b0a3ae740110ead5fab0732871e
amdadul-haque <amdadul1807107@gmail.com>
2024-05-19 19:41:27 +0600

    video added

--- components/hero-section.js
+++ components/hero-section.js
@@ -1,9 +1,16 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { motion } from 'framer-motion'
 import AnimatedHeader from './animated-header'
 
 const HeroSection = () => {
+
+  const videoRef = useRef();
+  useEffect(() => {
+    videoRef.current.play();
+  }, []);
+
+
   return (
     <div className='bg-gradient-to-b from-[#000] to to-[#111] '>
       <section className='section relative flex items-center justify-center sfpro-medium bg-gradient-to-b from-[#222] to to-[#000]'>
@@ -35,29 +42,59 @@ const HeroSection = () => {
         </motion.div>
       </motion.section >
 
-      <section className='section xl:h-[1000px] flex items-center'>
+      {/* <section className='section xl:h-[1000px] flex items-center'>
         <div className='container-wrapper h-full flex items-center justify-center '>
           <AnimatedHeader>
             but what's the <br /> <span className='text-[#7155eb] text-gradient'> difference</span>
           </AnimatedHeader>
         </div>
-      </section>
-      <section className='section xl:h-[1000px] flex items-center bg-video'>
-        <div className='container-wrapper h-full flex items-center justify-center'>
-          {/* <motion.div
-            initial={{ opacity: 0, y: 150 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: .6 }}
+      </section> */}
+
+      <section className='flex items-center py-10 sm:py-16 md:py-20 lg:py-28'>
+        <motion.h2 className="section-header !leading-[60%]"
+          initial={{ opacity: [0, 0, 0, 0, 0], y: 150 }}
+          whileInView={{ opacity: [0, 0.1, 0.3, 0.5, 1], y: 0 }}
+          transition={{ duration: .6 }}
+        >
+          <span className="normal-case">
+            But what's the  difference <br />
+            <span className='text-[50%] leading-[0px]'>Better pressure reliefe & better motiin absorbtion</span>
+          </span>
+        </motion.h2>
+      </section >
+
+      <div id="start" className="w-full xl:h-[100vh] bg-gray-950 text-white flex items-center justify-center text-center">
+        <div className="w-full z-0 relative h-full">
+          <video
+            ref={videoRef}
+            autoplay
+            loop
+            muted
+            className="object-cover w-full h-full aspect-[4/3] sm:aspect-video"
           >
-            <h2 className='section-header'> </h2>
-          </motion.div> */}
+            <source src="./video.mp4" type="video/mp4"></source>
+          </video>
+          <motion.div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full mx-auto z-10"
+            animate={{ opacity: 1 }}
+            initial={{ opacity: 0 }}
+            transition={{ duration: 1 }}
+          >
+            {/* <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-center leading-5">
+              Szkolenia Medialne <br /> Mistrzowie Mediów <br />
+            </h1> */}
+          </motion.div>
+        </div>
+      </div >
+
+      {/* <section className='section xl:h-[1000px] flex items-center bg-video'>
+        <div className='container-wrapper h-full flex items-center justify-center'>
           <AnimatedHeader>
             better pressure reliefe & <br />
             <span className='text-[50%]'>better motiin absorbtion</span>
           </AnimatedHeader>
         </div>
-      </section>
-      
+      </section> */}
+
     </div >
   )
 }
